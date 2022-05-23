be4b1f8634a0341aebc3574db1cd31c7fe95e1d1
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-05-23 10:58:27 +0300

    Changing variable name

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const header = document.querySelector(".container > h1"),
     dices = document.querySelectorAll(".container .dice img");
 
-var choices = []
+var selections = []
 
 dices.forEach(dice => {
 
   var dice_random_num = Math.floor(Math.random() * 6) +1;
-  choices.push(dice_random_num)
+  selections.push(dice_random_num)
   dice.src = `images/dice${dice_random_num}.png`;
 })
 
-if (choices[0] === choices[1]) {
+if (selections[0] === selections[1]) {
   header.innerText = "Draw!"
 }
 else {
-  header.innerText = `🚩Play ${choices.indexOf(Math.max(...choices)) +1} Wins!`
+  header.innerText = `🚩Play ${selections.indexOf(Math.max(...selections)) +1} Wins!`
 }
